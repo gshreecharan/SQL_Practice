138df983bd469f838fff0deba45fab0ea04cdd58
gshreecharan <gshreecharan.tej@gmail.com>
2026-03-03 15:18:56 +0530

    small changes updated

--- 1_create_Mystudents.sql
+++ 1_create_Mystudents.sql
@@ -4,3 +4,11 @@ create table Mystudents
   name varchar2(1000),
   class number(2)
 );
+
+create table students (
+    student_id number(5),
+    name varchar2(50),
+    email varchar2(100),
+    age number(3),
+    course varchar2(50)
+);
--- 3_Truncate_Mystudents.sql
+++ 3_Truncate_Mystudents.sql
@@ -1,2 +1,13 @@
 ---insert into Mystudents values(1,'charan',7);
 Truncate Table Mystudents;
+
+create table employees (
+    emp_id varchar2(10),
+    emp_name varchar2(50),
+    department varchar2(50),
+    salary number(10),
+    join_date date
+);
+
+
+Truncate Table Mystudents;
